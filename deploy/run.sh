068314a8548a7b17fc69e9e1113563e1adcb7132
John ODonnell <john.odonnell@cyberark.com>
2024-03-07 09:29:04 -0500

    Better logging around reaching ESO uninstall consensus

--- deploy/run.sh
+++ deploy/run.sh
@@ -84,9 +84,9 @@ if [[ "$PLATFORM" == "openshift" ]]; then
     "system:serviceaccount:$ESO_NAMESPACE_NAME:external-secrets"
 fi
 
-# ESO Helm chart can only be installed once per K8s cluster. This is causing
-# failures when pipelines are build simultaneously. We need to install the ESO
-# Helm chart to a single, predetermined namespace, and reach a consensus before
+# ESO Helm chart can only be installed once per K8s cluster, which caused
+# failures when pipelines are built simultaneously. Given this, we need to
+# install the ESO Helm chart to a single namespace, and reach a consensus before
 # uninstalling it.
 if helm list -n "$ESO_NAMESPACE_NAME" | grep -q eso; then
   echo "ESO Helm chart already installed - skipping"
@@ -99,6 +99,9 @@ else
     --values "./deploy/eso/values.${PLATFORM}.yml"
 fi
 
+# The mechanism to determine consensus are unique labels on the ESO namespace,
+# formatted 'conjur.org/$UNIQUE_TEST_ID'. Each pipeline will add its own unique
+# label set to 'PENDING', and set it to 'DONE' once it consents to uninstalling.
 if [[ "$DEV" == "false" ]]; then
   $cli label namespace "$ESO_NAMESPACE_NAME" "conjur.org/$UNIQUE_TEST_ID=PENDING"
 fi
